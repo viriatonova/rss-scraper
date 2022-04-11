da884abe77f04610f6ad6dd683c9f73c0849486c
viriatonova <viriatosampaio@gmail.com>
2022-04-11 16:49:50 -0300

    db working

--- api/server.js
+++ api/server.js
@@ -2,16 +2,25 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 53000;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTMONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.emit('Successfuly to connect MongoDB');
+}).catch(e => console.log(e));
+
+
 const routes = require('./routes');
 const { middlewareGlobal } = require('./src/middleware/middlewares.js');
-
-// ROUTERS 
 app.use(routes);
-
-//MIDDLEWARES
 app.use(middlewareGlobal)
 
-app.listen(port, () => {
-    console.log(`Servidor executando na porta ${port}`);
-    console.log(`Acesso ao servidor: http://localhost:${port}`);
+
+app.on('Successfuly to connect MongoDB', () => {
+    app.listen(port, () => {
+        console.log(`Servidor executando na porta ${port}`);
+        console.log(`Acesso ao servidor: http://localhost:${port}`);
+    });
 });
