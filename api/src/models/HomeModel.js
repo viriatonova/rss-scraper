7633d9bb7e922f6e33937f63222890645dbe3926
viriatonova <viriatosampaio@gmail.com>
2022-04-14 09:13:32 -0300

    get URLS

--- api/src/models/HomeModel.js
+++ api/src/models/HomeModel.js
@@ -1,18 +1,19 @@
 const mongose = require('mongoose');
 const fetch = require('node-fetch');
 const xml2js = require('xml2js');
-const fs = require('fs');
-const { get } = require('http');
 
 
-
+/**
+ * Mongose Schema for MongoDB
+ */
 const HomeSchema = new mongose.Schema({
     nome: { type: String, required: true },
-    last_edited: { type: Number, required: true},
+    last_edited: { type: Number, required: true },
     url: { type: String, required: true }
 });
 
 const HomeModel = mongose.model('Home', HomeSchema)
+
 
 class Home {
     constructor(body, url) {
@@ -20,7 +21,11 @@ class Home {
         this.url = url
     };
 
-    async getRSS () {
+    /**
+     * Fetch XML data from some URL
+     * @returns xml text
+     */
+    async getRSS() {
         try {
             const parser = new xml2js.Parser()
             const response = await fetch(this.url);
@@ -32,18 +37,23 @@ class Home {
         }
     };
 
-    
-   async getSitesData() {
+    /**
+     * Use {this.getRSS} for fetch data and select source URL's from 
+     * the feeds
+     * 
+     * @returns Object whitch time of check and source URL's from feeds 
+     */
+    async getSitesData() {
         const data = await this.getRSS(this.url)
         const items = await data.rss.channel[0].item
 
         this.body = {
             checkDate: data.rss.channel.lastBuildDate,
-            urls:[]
+            urls: []
         }
 
         for (let key in items) {
-            this.body.urls.push(items[key])
+            this.body.urls.push(items[key].source[0].$.url)
         }
 
         return this.body
